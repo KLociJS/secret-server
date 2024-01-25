9648f8b323688575c3db7258db25ee5d101c50f4
KLociJS <kisznerlorant21@gmail.com>
2024-01-25 16:37:29 +0100

    Add validator for remaining views

--- server/server/serializer.py
+++ server/server/serializer.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from .models import Secret
 
-
+def is_remaining_views_valid(value):
+    if value < 1:
+        raise serializers.ValidationError(
+            'Remaining views must be greater than 0.'
+        )
+    
 class SecretSerializer(serializers.ModelSerializer):
+    remaining_views = serializers.IntegerField(
+        validators=[is_remaining_views_valid]
+    )
     class Meta:
         model = Secret
         fields = ('__all__')
