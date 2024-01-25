2bb39f673c5f824f4f8b9e39f8fac18cb6e1ac4d
KLociJS <kisznerlorant21@gmail.com>
2024-01-25 16:22:21 +0100

    Write documentation

--- server/server/models.py
+++ server/server/models.py
@@ -5,6 +5,7 @@ from encrypted_model_fields.fields import EncryptedTextField
 
 
 class Secret(models.Model):
+    """ Model for storing secrets. """
     hash = ShortUUIDField(
         length=16,
         max_length=23,
@@ -17,18 +18,24 @@ class Secret(models.Model):
     remaining_views = models.IntegerField()
 
     def is_available(self):
-
+        """ Checks if the secret is not expired and has remaining views. """
         expiration_date = self.created_at + timedelta(minutes=self.expires_in)
 
+        # Check if the secret has no expiration date.
         has_no_expiration = self.expires_in == 0
+        # Check if the secret is not expired.
         is_not_expired = (
             expiration_date.timestamp() > datetime.now().timestamp()
         )
+        # Check if the secret has remaining views.
         has_remaining_views = self.remaining_views > 0
 
+        # The secret is available if it has remaining views and is not expired
         is_available = has_remaining_views and (
-            is_not_expired or has_no_expiration)
+            is_not_expired or has_no_expiration
+        )
 
+        # Decrease the remaining views if the secret is available.
         if is_available:
             self.remaining_views -= 1
             self.save()
--- server/server/views.py
+++ server/server/views.py
@@ -11,14 +11,33 @@ from .models import Secret
 
 @api_view(['POST'])
 def post_secret(request):
+    """
+    Create a new secret.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+        request data has to contain the following fields:
+            secret (str): The secret to be stored.
+            expireAfterViews (int):
+            Number of views before the secret is unavailable.
+            expireAfter (int): The secret won't be available
+            after the given time.
+            The value is provided in minutes. 0 means never expires
+
+    Returns:
+        Response: Serialized secret or an error message
+        if the request data is invalid.
+    """
 
     if request.method == 'POST':
         serializer = SecretSerializer(data=request.data)
 
+        # Check if the serializer is valid.
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+        # Return 405 if the serializer is not valid.
         return Response(
             {'error': 'Invalid input'},
             status=status.HTTP_405_METHOD_NOT_ALLOWED
@@ -28,10 +47,22 @@ def post_secret(request):
 @api_view(['GET'])
 @renderer_classes([JSONRenderer, XMLRenderer, YAMLRenderer])
 def get_secret(request, hash):
+    """
+    Retrieve a secret based on its hash.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+        hash (str): The hash identifying the secret.
+
+    Returns:
+        Response: Serialized secret or an error message
+        if the secret is not valid or not found.
+    """
 
     if request.method == 'GET':
         secret = get_object_or_404(Secret, hash=hash)
 
+        # Check if the secret is not expired and has remaining views.
         if secret.is_available():
             serializer = SecretSerializer(secret)
             return Response(
@@ -40,6 +71,7 @@ def get_secret(request, hash):
                 headers={'Content-Type': request.accepted_media_type}
             )
 
+        # Return 404 if the secret is expired or has no remaining views.
         return Response(
             {'error': 'Secret not available'},
             status=status.HTTP_404_NOT_FOUND
